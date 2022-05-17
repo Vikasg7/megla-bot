228464bb32bf195ba96bee4e406b59cd6d62ff54
Vikas <v33vikasgautam@gmail.com>
2022-05-18 03:20:09 +0530

    added try catch in weather.js

--- src/bots/weather.js
+++ src/bots/weather.js
@@ -1,9 +1,13 @@
 // !weather location
 async function weatherBot({ username }, ...args) {
-   const location = encodeURIComponent(args.join(" "))
-   const resp = await fetch(`https://wttr.in/${location}?format=4`)
-   const text = await resp.text()
-   return `@${username} ${text}`
+   try {
+      const location = encodeURIComponent(args.join(" "))
+      const resp = await fetch(`https://wttr.in/${location}?format=4`)
+      const text = await resp.text()
+      return `@${username} ${text}`
+   } catch (e) {
+      return `@${username} Oops! Error fetching weather.`
+   }
 }
 
 module.exports = weatherBot
